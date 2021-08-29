0005de082da7a4f0b680446e353c7e37c430a733
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-08-30 11:05:57 +1200

    transfer between

--- fn_farm_update_alt2.jl
+++ fn_farm_update_alt2.jl
@@ -1,11 +1,18 @@
-function farm_update_agent!(FarmAgent)
+function farm_update_agent!(FarmAgent, farmModel)
+
+farmno = FarmAgent.id
 
 animalModel = FarmAgent.animalModel
 
-
-
 run!(animalModel, agent_step!, model_step!, 1)
 
-println(animalModel.sending)
+       trade_partners = node_neighbors(FarmAgent, farmModel)
+        trade_partner = rand(1:length(trade_partners))
+        if trade_partner == farmno 
+            println("EXIT!") 
+        elseif length(animalModel.sending) != 0 && trade_partner != farmno
+            farmModel[trade_partner].animalModel.receiving = animalModel.sending
+           # println("traded") 
+        end 
 
 end
