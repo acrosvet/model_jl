c5d05b2e9ed2d08f08c028313d9d3ea85dd5bd82
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-08-30 11:50:53 +1200

    functional approach, adding agent

--- fn_farm_contact.jl
+++ fn_farm_contact.jl
@@ -1,9 +1,23 @@
 
 function contact!(FarmAgent, farmModel)
-    
-    #submodel = agent.submodel
 
-    trade_partners = node_neighbors(FarmAgent, farmModel)
+        farmno = FarmAgent.id
 
-    trade_partner = rand(1:length(trade_partners))
+        trade_partners = node_neighbors(FarmAgent, farmModel)
+
+       # println(trade_partners)
+        
+        trade_partner = rand(1:length(trade_partners))
+
+        while trade_partner == farmno
+            trade_partner = rand(1:length(trade_partners))
+            break
+        end
+
+        println(trade_partner)
+
+        farmModel[trade_partner].animalModel.receiving = FarmAgent.animalModel.sending
+
+        println(FarmAgent.animalModel.sending)
+        println(farmModel[trade_partner].animalModel.receiving)
 end
--- fn_farm_update_alt2.jl
+++ fn_farm_update_alt2.jl
@@ -6,13 +6,5 @@ animalModel = FarmAgent.animalModel
 
 run!(animalModel, agent_step!, model_step!, 1)
 
-#=        trade_partners = node_neighbors(FarmAgent, farmModel)
-        trade_partner = rand(1:length(trade_partners))
-        if trade_partner == farmno 
-            println("EXIT!") 
-        elseif length(animalModel.sending) != 0 && trade_partner != farmno
-            farmModel[trade_partner].animalModel.receiving = animalModel.sending
-           # println("traded") 
-        end  =#
 
 end
