d403a2e6f44b3d56605a1c3561fe6340efbf4793
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-08-30 10:57:42 +1200

    further simplification

--- fn_farm_update_alt2.jl
+++ fn_farm_update_alt2.jl
@@ -2,9 +2,10 @@ function farm_update_agent!(FarmAgent)
 
 animalModel = FarmAgent.animalModel
 
-include("astep_animal.jl")
-include("mstep_animal.jl")
+
 
 run!(animalModel, agent_step!, model_step!, 1)
+
+println(animalModel.sending)
 
 end
