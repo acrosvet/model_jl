f39705c94e57e1b96628740523b65666c68c6c82
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-08-30 17:28:34 +1200

    run to step

--- fn_farm_transmit.jl
+++ fn_farm_transmit.jl
@@ -4,5 +4,18 @@ function farm_transmit!(FarmAgent, farmModel)
 println(length(FarmAgent.trades_to))
  =#
 
+#=  if length(FarmAgent.trades_to) != 0
+    for i in 1:length(FarmAgent.trades_to)
+        agent = FarmAgent.trades_to[i]
+        println(agent)
+        newid = rand(5000:10000)
+        agent.id = newid
+        #agent.pos = Tuple(10*rand(FarmAgent.animalModel.rng, 2))
+        #Works until the second timestep
+        add_agent!(agent, FarmAgent.animalModel)
+        println("This worked")
+    end  =#
+#end
+
 
 end
