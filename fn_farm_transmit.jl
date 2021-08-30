870282a0bb546500e24f8a084050a860534a43db
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-08-30 15:38:18 +1200

    breaks on iteration

--- fn_farm_transmit.jl
+++ fn_farm_transmit.jl
@@ -1,5 +1,5 @@
 function farm_transmit!(FarmAgent, farmModel)
-
+#= 
 println(typeof(FarmAgent.trades_to))
 println(length(FarmAgent.trades_to))
 
@@ -7,9 +7,10 @@ if length(FarmAgent.trades_to) != 0
     for i in 1:length(FarmAgent.trades_to)
         agent = FarmAgent.trades_to[i]
         println(agent)
+        #Works until the second timestep
         add_agent!(agent, FarmAgent.animalModel)
         println("This worked")
     end
-end
+end =#
 
 end
