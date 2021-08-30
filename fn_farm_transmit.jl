218dad50852f3f7e73da45da1575e2366b0d0a20
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-08-30 15:02:51 +1200

    Trade back down from agent

--- fn_farm_transmit.jl
+++ fn_farm_transmit.jl
@@ -1,3 +1,8 @@
 function farm_transmit!(FarmAgent, farmModel)
 
+if length(FarmAgent.trades_to) != 0
+    for i in 1:length(FarmAgent.trades_to)
+        add_agent!(FarmAgent.animalModel, FarmAgent.trades_to[i])
+        println("This worked")
+    end
 end
