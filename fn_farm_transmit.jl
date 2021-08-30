7a70321fab34efa6cd104db148b799b2a17219f6
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-08-30 15:39:35 +1200

    trying with newid

--- fn_farm_transmit.jl
+++ fn_farm_transmit.jl
@@ -1,5 +1,5 @@
 function farm_transmit!(FarmAgent, farmModel)
-#= 
+
 println(typeof(FarmAgent.trades_to))
 println(length(FarmAgent.trades_to))
 
@@ -7,10 +7,12 @@ if length(FarmAgent.trades_to) != 0
     for i in 1:length(FarmAgent.trades_to)
         agent = FarmAgent.trades_to[i]
         println(agent)
+        newid = rand(5000:10000)
+        agent.id = newid
         #Works until the second timestep
         add_agent!(agent, FarmAgent.animalModel)
         println("This worked")
     end
-end =#
+end
 
 end
