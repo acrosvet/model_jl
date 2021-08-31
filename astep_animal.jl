068ffb90d977cb2b74a9c344342bb8834fe97ef5
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-01 08:27:31 +1200

    Reintroduced mortality, code comments

--- astep_animal.jl
+++ astep_animal.jl
@@ -3,14 +3,14 @@
         move_agent!(AnimalAgent, animalModel, animalModel.timestep) #Move the agent in space
         treatment!(AnimalAgent, animalModel) #Introduce treatment
         treatment_effect!(AnimalAgent) #Effect of treatment on transmission.
-        endTreatment!(AnimalAgent, animalModel)
+        endTreatment!(AnimalAgent, animalModel) #End treatment
         retreatment!(AnimalAgent, animalModel) #Effect of retreatment
-        #mortality!(AnimalAgent, animalModel) #Introduce mortality
+        mortality!(AnimalAgent, animalModel) #Introduce mortality
         recover!(AnimalAgent, animalModel) # Introduce recovery
         carrierState!(AnimalAgent, animalModel) #Introduce a carrier state
         update_agent!(AnimalAgent) #Apply the update_agent function
-        run_submodel!(AnimalAgent, animalModel)
-        bacto_dyno!(AnimalAgent)
+        run_submodel!(AnimalAgent, animalModel) #Run the bacterial submodel
+        bacto_dyno!(AnimalAgent) #Determine the overall bacterial dynamics for an animal
 
     end
  
