964f3f6c41a7daf3eef5a3fa3385bb4fbe8f9cc4
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-01 12:32:11 +1200

    added current_heifers counter

---
+++ fn_trading_need.jl
@@ -0,0 +1,28 @@
+"""
+ trading_need!(animalModel)
+ * Determines percentage variation of current herd size from initial condition, and whether a herd may need to acquire more animals
+"""
+function trading_need!(animalModel)
+
+    optimal_size = animalModel.herd_size
+
+    current_size = length(animalModel.agents)
+
+    optimal_heifers = 0.3*optimal_size
+
+    optimal_weaned = 0.3*optimal_size
+
+    function current_heifers(animalModel)
+        counter = 0
+        for i in 1:length(animalModel.agents)
+            if animalModel.agents[i].stage == :H
+                counter += 1
+            end
+        end
+        return counter
+    end
+
+    current_heifers = current_heifers(animalModel)
+
+
+end
