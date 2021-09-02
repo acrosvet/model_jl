7228e09e3d6618a2485ab87e821afffe54a84747
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-02 14:00:12 +1200

    added in additional trading classes

--- mstep_farm.jl
+++ mstep_farm.jl
@@ -23,6 +23,8 @@ function farm_step!(FarmAgent, farmModel)
     #    println(length(FarmAgent.animalModel.sending))        
         agents_to_remove = []
         
+# Trade heifers ----------------------------------------
+
         if FarmAgent.animalModel.tradeable_heifers < 0 && farmModel[trade_partner].animalModel.tradeable_heifers > 0
             println("let's trade heifers!")
 
@@ -65,6 +67,97 @@ function farm_step!(FarmAgent, farmModel)
 #            println("This many heifers were sent:")
 #            println(length(farmModel[trade_partner].animalModel.receiving))
         end
+
+# Trade lactating  ----------------------------------------
+
+if FarmAgent.animalModel.tradeable_lactating < 0 && farmModel[trade_partner].animalModel.tradeable_lactating > 0
+    println("let's trade heifers!")
+
+    FarmAgent.trades_from = FarmAgent.animalModel.sending
+
+    lactating_to_send = []
+
+    if FarmAgent.animalModel.sending == 0
+#             println("No agents to send")
+    else
+        for i in 1:length(FarmAgent.animalModel.sending)
+            if FarmAgent.animalModel.sending[i].stage == :L | FarmAgent.animalModel.sending[i].stage == :D
+                push!(lactating_to_send, FarmAgent.animalModel.sending[i])
+            else
+#                      println("No lactating to send")
+            end
+        end
+    end
+
+#           println("The candidate number of heifers to send is ")
+#           println(length(heifers_to_send))
+
+    num_trades_to = abs(FarmAgent.animalModel.tradeable_lactating) ≤ length(heifers_to_send) ? abs(FarmAgent.animalModel.tradeable_lactating) : length(lactating_to_send)
+
+
+#            println("Number of trades to is $num_trades_to")
+
+
+
+    for i in 1:num_trades_to
+        if length(lactating_to_send) != 0
+            push!(farmModel[trade_partner].animalModel.receiving, lactating_to_send[i]) 
+            println("Lactating cow traded to destination herd")
+            push!(agents_to_remove, lactating_to_send[i])
+            println("Lactating cow sent to purge list")
+        else
+#                   println("No heifers to send")
+        end
+    end
+#            println("This many heifers were sent:")
+#            println(length(farmModel[trade_partner].animalModel.receiving))
+end
+        
+# Trade weaned ----------------------------------------
+
+if FarmAgent.animalModel.tradeable_weaned < 0 && farmModel[trade_partner].animalModel.tradeable_weaned > 0
+    println("let's trade weaned!")
+
+    FarmAgent.trades_from = FarmAgent.animalModel.sending
+
+    weaned_to_send = []
+
+    if FarmAgent.animalModel.sending == 0
+#             println("No agents to send")
+    else
+        for i in 1:length(FarmAgent.animalModel.sending)
+            if FarmAgent.animalModel.sending[i].stage == :W
+                push!(weaned_to_send, FarmAgent.animalModel.sending[i])
+            else
+#                      println("No weaned to send")
+            end
+        end
+    end
+
+#           println("The candidate number of weaned to send is ")
+#           println(length(weaned_to_send))
+
+    num_trades_to = abs(FarmAgent.animalModel.tradeable_weaned) ≤ length(weaned_to_send) ? abs(FarmAgent.animalModel.tradeable_weaned) : length(weaned_to_send)
+
+
+#            println("Number of trades to is $num_trades_to")
+
+
+
+    for i in 1:num_trades_to
+        if length(weaned_to_send) != 0
+            push!(farmModel[trade_partner].animalModel.receiving, weaned_to_send[i]) 
+            println("Heifer traded to destination herd")
+            push!(agents_to_remove, weaned_to_send[i])
+            println("Heifer sent to purge list")
+        else
+#                   println("No weaned to send")
+        end
+    end
+#            println("This many weaned were sent:")
+#            println(length(farmModel[trade_partner].animalModel.receiving))
+end
+
 
 # Remove the traded agents
         for i in 1:length(agents_to_remove)
