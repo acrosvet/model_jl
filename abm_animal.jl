bb6a471a5f55067179dc6f83712ea893975d8797
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-06 08:34:29 +1200

    changed the argument order

--- abm_animal.jl
+++ abm_animal.jl
@@ -176,7 +176,8 @@
         days_exposed = 0
         days_carrier = 0
         trade_status = false
-        add_agent!(pos, animalModel, vel, age, status, βₛ, βᵣ, inf_days_is, inf_days_ir, treatment, days_treated, since_tx, bactopop, submodel, stage, dim, days_dry, trade_status, days_exposed, days_carrier)   
+        add_agent!(pos, animalModel, vel, age, status, βₛ, βᵣ, inf_days_is, inf_days_ir, days_exposed, days_carrier, treatment, days_treated, since_tx, bactopop, submodel, stage, dim, days_dry, trade_status)
+        #add_agent!(pos, animalModel, vel, age, status, βₛ, βᵣ, inf_days_is, inf_days_ir, treatment, days_treated, since_tx, bactopop, submodel, stage, dim, days_dry, trade_status, days_exposed, days_carrier)   
     
     end
 
