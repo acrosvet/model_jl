6e6fe7debbe9341f1e8bb2e13a2989495aad6b1d
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-06 11:12:38 +1200

    point to the right DataFrame

---
+++ export_animal_data.jl
@@ -0,0 +1,23 @@
+function export_animal_data!(AnimalAgent, animalModel)
+
+data = DataFrame(
+    Day = animalModel.calday,
+    AnimalID = AnimalAgent.id,
+    AnimalStatus = AnimalAgent.status,
+    AnimalStage = AnimalAgent.stage,
+    DaysInfected = AnimalAgent.inf_days,
+    DaysExposed = AnimalAgent.days_exposed,
+    DaysCarrier = AnimalAgent.days_carrier,
+    AnimalTreatment = AnimalAgent.treatment,
+    DaysTreated = AnimalAgent.days_treated,
+    DaysSinceTreatment = AnimalAgent.since_tx,
+    DaysDry = AnimalAgent.days_dry,
+    TradeStatus = AnimalAgent.trade_status,
+    AnimalBactoPop = AnimalAgent.bactopop
+)
+
+output = open("./export/animal_model_run.csv","a")
+    CSV.write(output, data, delim = ";", append = true, header = false)
+    close(output)
+
+end
