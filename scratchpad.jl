bfca491fc4a7a545332af51773550e00ffae0275
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-06 11:24:59 +1200

    non-functional change

--- scratchpad.jl
+++ scratchpad.jl
@@ -1,3 +1,24 @@
 tmp = initialiseModel(100)
 
-step!(tmp, agent_step!, model_step!, 10)
+
+header = DataFrame(
+    Day = 0,
+    AnimalID = 0,
+    AnimalStatus = 0,
+    AnimalStage = 0,
+    DaysInfected = 0,
+    DaysExposed = 0,
+    DaysCarrier = 0,
+    AnimalTreatment = 0,
+    DaysTreated = 0,
+    DaysSinceTreatment = 0,
+    DaysDry = 0,
+    TradeStatus = 0,
+    AnimalBactoPop = 0
+)
+
+output = open("./export/animal_model_run.csv","a")
+    CSV.write(output, header, delim = ";", append = true, header = true)
+    close(output)
+
+step!(tmp, agent_step!, model_step!, 100)
