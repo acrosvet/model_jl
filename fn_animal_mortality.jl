a4f65b2d0e6541b8479ef9f6a85c433037f36cad
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-07 09:28:36 +1200

    temp deprecate disease related mortality

--- fn_animal_mortality.jl
+++ fn_animal_mortality.jl
@@ -1,12 +1,12 @@
 function mortality!(AnimalAgent, animalModel)
     # If an animal is infected sensitive, cull if it is eligible to be culled
-    if AnimalAgent.status == :IS && (rand(animalModel.rng) < animalModel.mortalityRateSens)
+#=     if AnimalAgent.status == :IS && (rand(animalModel.rng) < animalModel.mortalityRateSens)
         kill_agent!(AnimalAgent, animalModel)
     # The same for animals infected with resistant bacteria, according to that mortality rate
     elseif AnimalAgent.status == :IR && (rand(animalModel.rng) < animalModel.mortalityRateRes)
         kill_agent!(AnimalAgent, animalModel)
     end
-
+ =#
     # Cull agent -------------------------------
 
     # Cull cows that have been in the herd for too long at a 30% replacement rate
