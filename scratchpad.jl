67ff5af846c0771602d51c17c2debf6cd6daa75c
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-07 11:01:33 +1200

    loop code

--- scratchpad.jl
+++ scratchpad.jl
@@ -17,10 +17,19 @@ header = DataFrame(
     TradeStatus = 0,
     AnimalBactoPop = 0,
     ModelYear = 0,
+    AnimalAge = 0
 )
 
-output = open("./export/farm_$farm_id-animal_model_run.csv","w")
+output = open("./export/animal_model_run.csv","w")
     CSV.write(output, header, delim = ",", append = true, header = true)
     close(output)
 
-step!(tmp, agent_step!, model_step!, 10*365)
+run!(tmp, agent_step!, model_step!, 10*365)
+
+for _ in 1:100
+    out = tmp[10].stage == :W && (tmp[10].age == rand(truncated(Poisson(13*30), 13*30, (13*30 + 9*7))))
+    tmp[10].age +=1
+    if out == true
+        println("Condition met")
+    end
+end
