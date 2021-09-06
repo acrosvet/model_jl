71a7270963fdba7f573c8f2f73a7565f1e733b6a
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-06 14:18:13 +1200

    updated culling age

--- fn_animal_mortality.jl
+++ fn_animal_mortality.jl
@@ -10,7 +10,7 @@ function mortality!(AnimalAgent, animalModel)
     # Cull agent -------------------------------
 
     # Cull cows that have been in the herd for too long at a 30% replacement rate
-    if (AnimalAgent.stage == :L && AnimalAgent.age == rand(truncated(Poisson(5*365), 13*30, 8*365),10000)) && (animalModel.culling_rate/365 > rand(animalModel.rng)/365)
+    if (AnimalAgent.stage == :L && AnimalAgent.age == rand(truncated(Poisson(floor(7.5*365)), 2*365, 10*365))) && (animalModel.culling_rate/365 > rand(animalModel.rng)/365)
         kill_agent!(AnimalAgent, animalModel)
         println("Cow culled!")
     end
