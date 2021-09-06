d901959318571c3625ff8229c1003dbd00abab1e
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-07 11:57:25 +1200

    initialise lactation

--- abm_animal.jl
+++ abm_animal.jl
@@ -166,7 +166,8 @@
         days_exposed = 0 # Default 0 
         days_carrier = 0 # Default 0 
         trade_status = false
-        add_agent!(pos, animalModel, vel, age, status, βₛ, βᵣ, inf_days, days_exposed, days_carrier, treatment, days_treated, since_tx, bactopop, submodel, stage, dim, days_dry, trade_status, agenttype)
+        lactation = 0
+        add_agent!(pos, animalModel, vel, age, status, βₛ, βᵣ, inf_days, days_exposed, days_carrier, treatment, days_treated, since_tx, bactopop, submodel, stage, dim, days_dry, trade_status, agenttype, lactation)
     
     end
 
