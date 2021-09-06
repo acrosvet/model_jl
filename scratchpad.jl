7921f63225193855416f4e82e2696bd9b5593f0c
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-06 14:40:52 +1200

    run for 10 years

--- scratchpad.jl
+++ scratchpad.jl
@@ -14,11 +14,12 @@ header = DataFrame(
     DaysSinceTreatment = 0,
     DaysDry = 0,
     TradeStatus = 0,
-    AnimalBactoPop = 0
+    AnimalBactoPop = 0,
+    ModelYear = 0,
 )
 
 output = open("./export/animal_model_run.csv","w")
     CSV.write(output, header, delim = ";", append = true, header = true)
     close(output)
 
-step!(tmp, agent_step!, model_step!, 1825)
+step!(tmp, agent_step!, model_step!, 10*365)
