f6e458c51bc9c61cbee6bd520c9ed889bd92c982
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 09:17:43 +1200

    Create docstring

--- calving.jl
+++ calving.jl
@@ -1,7 +1,22 @@
-function calving!(AnimalAgent, animalModel)
+"""
+**calving!(AnimalAgent, animalModel):**\n
 
-    if animalModel.date ≥ animalModel.psc
-        
+Calve cows based on their number of days in calf (AnimalAgent.dic) within ± 14 days of the gestation length of 283 days.\n
+
+After calving:
+* Change the days in calf to 0
+* Change the pregnancy status to E
+* Change the stage to lactating
+* Set the days in milk to 0
+* Increment the lactation number
+
+"""
+function calving!(AnimalAgent)
+    if AnimalAgent.dic == 283 + rand(-14:1:14)
+        AnimalAgent.pregstat = :E
+        AnimalAgent.dic = 0
+        AnimalAgent.stage = :L
+        AnimalAgent.dim = 0
+        AnimalAgent.lactation += 1
     end
-
 end
