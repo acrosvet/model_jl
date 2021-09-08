45cd103f520e701fbbd1c60b4458c9404b8a3871
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-08 12:01:21 +1200

    add current_lac

--- abm_animal.jl
+++ abm_animal.jl
@@ -62,9 +62,7 @@
         tradeable_weaned = 0, # Ibid, weand
         tradeable_stock = 0, # Ibid, all stock
         farm_id,
-        psc = 245, # Calday, planned start of calving
-        births = 0,
-        deaths = [],
+        current_lac = 0,
  )# Dictionary of disease properties
 
     # Define the model: Agent type, agent space, properties, and type of random seed
