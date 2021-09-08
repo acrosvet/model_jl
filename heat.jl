52daa3f974ccadefa50ed90334acda0e8021afb9
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 10:58:27 +1200

    create the heat function

---
+++ heat.jl
@@ -0,0 +1,16 @@
+"""
+**heat!(AnimalAgent)**\n
+
+* Give empty lactating animals a heat every 21 days
+
+"""
+
+function heat!(AnimalAgent)
+
+if AnimalAgent.pregstat == :E
+    if AnimalAgent.dim % 21 == 0
+        AnimalAgent.heat = true
+    end
+end
+
+end
