fb7ccbacc6f6c88870d96cc38166335d8a2af104
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 12:05:32 +1200

    AnimalAge to AnimalAgent.age

--- wean.jl
+++ wean.jl
@@ -7,7 +7,7 @@
 """
 function wean!(AnimalAgent, animalModel)
     if AnimalAgent.stage == :C
-        if AnimalAge ≥ rand(animalModel.rng, truncated(Poisson(60), 55, 70))
+        if AnimalAgent.age ≥ rand(animalModel.rng, truncated(Poisson(60), 55, 70))
             AnimalAgent.stage = :W
         end
     end
