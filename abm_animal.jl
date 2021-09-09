449f3ca5c6431afda1d41d87dd2bc0d2b280c7ca
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 12:55:06 +1200

    add sex

--- abm_animal.jl
+++ abm_animal.jl
@@ -135,7 +135,8 @@
         pregstat = :P
         dic = rand(animalModel.rng, truncated(Poisson(247), 199, 290)) #Gives a 63% ICR for this rng
         heat = false
-        add_agent!(pos, animalModel, vel, age, status, βₛ, βᵣ, inf_days, days_exposed, days_carrier, treatment, days_treated, since_tx, bactopop, submodel, stage, dim, days_dry, trade_status, agenttype, lactation, pregstat, dic, heat)
+        sex = :F
+        add_agent!(pos, animalModel, vel, age, status, βₛ, βᵣ, inf_days, days_exposed, days_carrier, treatment, days_treated, since_tx, bactopop, submodel, stage, dim, days_dry, trade_status, agenttype, lactation, pregstat, dic, heat, sex)
     
     end
 
