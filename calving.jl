32d962d924abd5c0c4cbc44a194431219daeeda4
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 12:55:47 +1200

    status to stage

--- calving.jl
+++ calving.jl
@@ -13,7 +13,7 @@ After calving:
 """
 function calving!(AnimalAgent, animalModel)
 
-    if AnimalAgent.dic ≥ 283 && AnimalAgent.stage == :D
+    if AnimalAgent.dic ≥ 283 && AnimalAgent.stage != :L
         AnimalAgent.pregstat = :E
         AnimalAgent.dic = 0
         AnimalAgent.stage = :L
--- scratchpad.jl
+++ scratchpad.jl
@@ -1,4 +1,4 @@
-tmp = initialiseModel(50)
+tmp = initialiseModel(100)
 
 
 header = DataFrame(
