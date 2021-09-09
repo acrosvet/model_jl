540295224bd796936a9e2298bd9daee70921481e
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 14:57:29 +1200

    reset DIM

--- dryoff.jl
+++ dryoff.jl
@@ -6,5 +6,6 @@ function dryoff!(AnimalAgent, animalModel)
     if AnimalAgent.dim ≥ rand(animalModel.rng, truncated(Poisson(305), 290, 330))
         AnimalAgent.stage = :D
         AnimalAgent.days_dry = 1
+        AnimalAgent.dim = 0
     end
 end
