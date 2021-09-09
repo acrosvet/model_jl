a411a861771ef06e779615ff3c55a7f8c106bd62
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 14:18:09 +1200

    more selective joining

--- heifer_joining.jl
+++ heifer_joining.jl
@@ -9,14 +9,14 @@ function heifer_joining!(AnimalAgent, animalModel)
     if AnimalAgent.stage == :H
         if (animalModel.date ≥ (animalModel.msd - Day(21))) && (animalModel.date ≤ (animalModel.msd + Day(9*7)))
             #if (AnimalAgent.age - 13*30 % 21 == 0)
-            #if ((animalModel.date - (animalModel.msd - Day(21))) % 21) == 0
+            if (Dates.value(animalModel.date - (animalModel.msd - Day(21))) % 21) == 0
                 if rand(animalModel.rng) > 0.5
                     AnimalAgent.pregstat = :P
                     AnimalAgent.dic = 1
                     AnimalAgent.stage = :DH
                     println("Heifer joined")
                 end
-           # end   
+            end   
         end
     end
 end
