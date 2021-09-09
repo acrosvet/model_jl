7a7b50a28f370f9a3a83e1d7f94b5e8530b16c5f
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-10 10:20:34 +1200

    introduce age-based culling

--- cull_milkers.jl
+++ cull_milkers.jl
@@ -7,6 +7,17 @@
 
 function cull_milkers!(AnimalAgent, animalModel)
 
+    function current_stock(animalModel, stage)
+        counter = 0
+        for i in 1:length(animalModel.agents)
+            if (haskey(animalModel.agents, i) == true) && animalModel.agents[i].stage == stage
+                counter += 1
+            end
+        end
+        return counter
+
+    end
+
 if AnimalAgent.stage == :D && AnimalAgent.pregstat == :E
     kill_agent!(AnimalAgent, animalModel)
     println("Culled empty dry")
@@ -16,7 +27,15 @@ if AnimalAgent.dic >= 320
     kill_agent!(AnimalAgent, animalModel)
     println("Slipped, cull")
 end
+
+current_lactating = current_stock(animalModel, :L)
     
+if current_lactating > animalModel.num_lac && AnimalAgent.dim ≥ 280
+    if AnimalAgent.age ≥ rand(animalModel.rng, truncated(Poisson(7*365), 2*365, 9*365))
+        kill_agent!(AnimalAgent, animalModel)
+        println("Age cull")
+    end
+end
 #=     if AnimalAgent.stage == :L && AnimalAgent.dim ≥ 280
         if AnimalAgent.pregstat == :E
                 kill_agent!(AnimalAgent, animalModel)
