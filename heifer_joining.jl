26e72537a369513252dbe6d3937a965089f2c285
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 13:32:46 +1200

    day to date

--- heifer_joining.jl
+++ heifer_joining.jl
@@ -8,7 +8,7 @@
 function heifer_joining!(AnimalAgent, animalModel)
     if AnimalAgent.stage == :H
         if (animalModel.date ≥ (animalModel.msd - Day(21))) && (animalModel.date ≤ (animalModel.msd + Day(9*7)))
-            if animalModel.day % (animalModel.msd - Day(21)) == 0
+            if animalModel.date % (animalModel.msd - Day(21)) == 0
                 if rand(animalModel.rng) > 0.5
                     animal.pregstat = :P
                     animal.dic = 1
