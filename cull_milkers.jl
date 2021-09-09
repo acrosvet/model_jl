a65754f1bc2eb2110d2bf802f61c4b5254d274cd
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 14:48:53 +1200

    simplified empty culls

--- cull_milkers.jl
+++ cull_milkers.jl
@@ -7,19 +7,17 @@
 
 function cull_milkers!(AnimalAgent, animalModel)
 
-if AnimalAgent.stage == :L && AnimalAgent.dim ≥ 280
-    if AnimalAgent.pregstat == :E
-        if animalModel.date ≥ (animalModel.msd + Day(12*7))
-            kill_agent!(AnimalAgent, animalModel)
-            println("Fertility cull")
+    if AnimalAgent.stage == :L && AnimalAgent.dim ≥ 280
+        if AnimalAgent.pregstat == :E
+                kill_agent!(AnimalAgent, animalModel)
+                println("Fertility cull")
+        
+    #=     elseif AnimalAgent.age ≥ rand(animalModel.rng, truncated(Poisson(7*365), 2*365, 9*365)) && AnimalAgent.dim ≥ 280
+            if rand(animalModel.rng) > 0.5
+                kill_agent!(AnimalAgent, animalModel)
+                println("Age cull")
+            end =#
         end
-    
-    elseif AnimalAgent.age ≥ rand(animalModel.rng, truncated(Poisson(7*365), 2*365, 9*365)) && AnimalAgent.dim ≥ 280
-        if rand(animalModel.rng) > 0.5
-            kill_agent!(AnimalAgent, animalModel)
-            println("Age cull")
-        end
-    end
-end    
+    end    
 
 end
