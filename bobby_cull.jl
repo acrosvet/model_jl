23b8a2f07c65489d847babf2d3606caa4751ec50
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-10 08:59:00 +1200

    remove printoutput

--- bobby_cull.jl
+++ bobby_cull.jl
@@ -7,7 +7,7 @@ function bobby_cull!(AnimalAgent, animalModel)
 
     if AnimalAgent.sex == :M && AnimalAgent.age > 3
         kill_agent!(AnimalAgent, animalModel)
-        println("Bobby cull!")
+        #println("Bobby cull!")
     end
 
 end
