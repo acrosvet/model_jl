77c673f0ac30ab409ad140af18e6ea70ccd6b71a
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 13:18:11 +1200

    add heifer joining

--- astep_animal.jl
+++ astep_animal.jl
@@ -24,6 +24,7 @@ Step AnimalAgents through time
         joining!(AnimalAgent, animalModel)
         wean!(AnimalAgent, animalModel)
         heifer!(AnimalAgent, animalModel)
+        heifer_joining!(AnimalAgent, animalModel)
         update_agent!(AnimalAgent) #Apply the update_agent function
         #flag_trades!(AnimalAgent, animalModel)
         export_animal_data!(AnimalAgent, animalModel)
