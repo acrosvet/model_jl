3b1308bab15242317054950154a087d3b7d0e20d
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-09 13:09:20 +1200

    bobby culling function

---
+++ bobby_cull.jl
@@ -0,0 +1,13 @@
+"""
+**bobby_cull!(AnimalAgent, animalModel)**
+* Remove bobby calves at three days
+"""
+
+function bobby_cull!(AnimalAgent, animalModel)
+
+    if AnimalAgent.sex == :M && AnimalAgent.age > 3
+        kill_agent!(AnimalAgent, animalModel)
+        println("Bobby cull!")
+    end
+
+end
