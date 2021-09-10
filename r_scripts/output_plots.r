e210beac7c7d8551b8fb37e7827c77d1e435ff10
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-10 16:44:05 +1200

    herd size growing

--- r_scripts/output_plots.r
+++ r_scripts/output_plots.r
@@ -1,8 +1,9 @@
 
+source("./r_scripts/libraries.r")
 # Import the model run from the animalModel
 run <- read_csv("/home/alex/Documents/julia_abm/model_jl/export/animal_model_run.csv")
 
-tmp = run %>% filter(DIM > 280)
+tmp = run %>% filter(DIM == 280)
 
 # Generate a ploot of population dynamics over time
 run %>% 
@@ -13,6 +14,10 @@ run %>%
   plot_ly() %>% 
   add_trace(x = ~ModelStep, y = ~L, type = 'bar', name = 'L') %>% 
   add_trace(x = ~ModelStep, y = ~D, type = 'bar', name = 'D') %>% 
+  add_trace(x = ~ModelStep, y = ~C, type = 'bar', name = 'C') %>%
+  add_trace(x = ~ModelStep, y = ~H, type = 'bar', name = 'H') %>%
+  add_trace(x = ~ModelStep, y = ~DH, type = 'bar', name = 'DH') %>%
+  add_trace(x = ~ModelStep, y = ~D, type = 'bar', name = 'W') %>%
   layout(barmode = 'stack')
 
   run %>% 
