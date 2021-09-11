af67096e8b938fd8011ee7cac6ee28746951e50c
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-11 20:17:07 +1200

    add split dynamics

--- joining.jl
+++ joining.jl
@@ -7,19 +7,22 @@
 
 """
 function joining!(AnimalAgent, animalModel)
-
-if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd + Month(3)))
-    if rand(animalModel.rng) < 0.85
-        AnimalAgent.pregstat = :P
-        AnimalAgent.dic = rand(animalModel.rng, truncated(Poisson(63), 1, 84))
+if AnimalAgent.calving_season == :Spring
+    if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd + Month(3)))
+        if rand(animalModel.rng) < 0.85
+            AnimalAgent.pregstat = :P
+            AnimalAgent.dic = rand(animalModel.rng, truncated(Poisson(63), 1, 84))
+        end
     end
 end
 
 if animalModel.system == :Split
-    if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd_2 + Month(3)))
-        if rand(animalModel.rng) < 0.85
-            AnimalAgent.pregstat = :P
-            AnimalAgent.dic = rand(animalModel.rng, truncated(Poisson(63), 1, 84))
+    if AnimalAgent.calving_season == :Autumn
+        if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd_2 + Month(3)))
+            if rand(animalModel.rng) < 0.85
+                AnimalAgent.pregstat = :P
+                AnimalAgent.dic = rand(animalModel.rng, truncated(Poisson(63), 1, 84))
+            end
         end
     end
 end
