7fdc036b5d4bcf66ff85b60ff63ab7db56992f17
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-11 18:14:02 +1200

    clean up commented code

--- joining.jl
+++ joining.jl
@@ -7,26 +7,6 @@
 
 """
 function joining!(AnimalAgent, animalModel)
-#= 
-    function heat_detection_efficiency(animalModel)
-        if animalModel.date ≤ (animalModel.msd + Day(21))
-            0.1 # 90% chance in the first few weeks of mating
-        else 
-            0.2 # 80% thereafter
-        end
-    end
-if AnimalAgent.pregstat == :E
-    if animalModel.date ≥ animalModel.msd && animalModel.date ≤ (animalModel.msd + Day(12*7)) # End mating at 12 weeks
-            if AnimalAgent.dim ≥ 28 && AnimalAgent.heat == true
-                if rand(animalModel.rng) > heat_detection_efficiency(animalModel) # Allow for a 90% chance of detecting heat in the first service
-                    if rand(animalModel.rng) > 0.5 # and a 50% chance of conception
-                        AnimalAgent.pregstat = :P 
-                        AnimalAgent.dic = 1                   
-                    end
-                end
-            end   
-    end
-end =#
 
 if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd + Month(3)))
     if rand(animalModel.rng) < 0.85
