3aa8efdce13506c98eac1c7ca112fa0101b2b795
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-12 20:09:01 +1200

    don't carry over twice

--- dryoff.jl
+++ dryoff.jl
@@ -5,12 +5,12 @@ function dryoff!(AnimalAgent, animalModel)
 
     if AnimalAgent.dim ≥ Int(floor(rand(animalModel.rng, truncated(Rayleigh(305), 290, 330)))) && AnimalAgent.agenttype != :CO
         if AnimalAgent.pregstat == :E
-            if rand(animalModel.rng) > 0.4
+            if rand(animalModel.rng) > 0.4 && AnimalAgent.dim < 500
                 if AnimalAgent.calving_season == :Spring
                     AnimalAgent.calving_season = :Autumn
                     AnimalAgent.agenttype = :CO
                     println("Carried over")
-                elseif AnimalAgent.calving_season == :Autumn
+                elseif AnimalAgent.calving_season == :Autumn && AnimalAgent.dim < 500
                     AnimalAgent.calving_season = :Spring
                     println("Carried over")
                     AnimalAgent.agenttype = :CO
