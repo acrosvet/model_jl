bccd22a0272ba9d3d3fa5c56f4d12235511fa0b4
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-13 12:24:01 +1200

    add batch and split options

--- joining.jl
+++ joining.jl
@@ -17,8 +17,10 @@ if AnimalAgent.calving_season == :Spring
     end
 end
 
-    if AnimalAgent.calving_season == :Autumn
-        if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd_2 + Month(3)))
+# Additional split calving options ---------------------------------
+if animalModel.system == :Split
+    if AnimalAgent.calving_season == :B1
+        if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd + Month(3)))
             if rand(animalModel.rng) < 0.85
                 AnimalAgent.pregstat = :P
                 AnimalAgent.dic = Int(floor(rand(animalModel.rng, truncated(Rayleigh(63), 1, 84))))
@@ -26,7 +28,45 @@ end
             end
         end
     end
+end
 
+# Additional batch calving options ------------------------------------------------
+
+if animalModel.system == :Batch
+    if AnimalAgent.calving_season == :B1
+        if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd + Month(3)))
+            if rand(animalModel.rng) < 0.85
+                AnimalAgent.pregstat = :P
+                AnimalAgent.dic = Int(floor(rand(animalModel.rng, truncated(Rayleigh(63), 1, 84))))
+                AnimalAgent.agenttype = :Joined
+            end
+        end
+    elseif AnimalAgent.calving_season == :B2
+        if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd_2 + Month(3)))
+            if rand(animalModel.rng) < 0.85
+                AnimalAgent.pregstat = :P
+                AnimalAgent.dic = Int(floor(rand(animalModel.rng, truncated(Rayleigh(63), 1, 84))))
+                AnimalAgent.agenttype = :Joined
+            end
+        end
+    elseif AnimalAgent.calving_season == :B3
+        if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd_3 + Month(3)))
+            if rand(animalModel.rng) < 0.85
+                AnimalAgent.pregstat = :P
+                AnimalAgent.dic = Int(floor(rand(animalModel.rng, truncated(Rayleigh(63), 1, 84))))
+                AnimalAgent.agenttype = :Joined
+            end
+        end
+    elseif AnimalAgent.calving_season == :B4
+        if (AnimalAgent.pregstat == :E && AnimalAgent.stage == :L) && (animalModel.date == (animalModel.msd_4 + Month(3)))
+            if rand(animalModel.rng) < 0.85
+                AnimalAgent.pregstat = :P
+                AnimalAgent.dic = Int(floor(rand(animalModel.rng, truncated(Rayleigh(63), 1, 84))))
+                AnimalAgent.agenttype = :Joined
+            end
+        end    
+    end
+end
 
 end
 
