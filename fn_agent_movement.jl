4cac6868e014b9008a1e409d8acbb514cb4ec55b
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-15 10:37:34 +1200

    move instead of walk

--- fn_agent_movement.jl
+++ fn_agent_movement.jl
@@ -1,7 +1,19 @@
 function agent_movement!(AnimalAgent, animalModel)
 
 if AnimalAgent.stage == :C
-    walk!(AnimalAgent, (rand(animalModel.rng(-10:10)), rand(animalModel.rng(-10:10)),0), animalModel; ifempty = false)
+    num_calves = [a.stage == :C for a in allagents(animalModel)]
+    num_calves = sum(num_calves)
+    if num_calves == 0
+        calf_range = 10
+    else
+        calf_range = Int(floor(3*√num_calves))
+    end 
+    pos = (rand(animalModel.rng, 1:calf_range, 2)..., 1)
+    while !isempty(pos, animalModel)
+        pos = (rand(animalModel.rng, 1:calf_range, 2)..., 1)
+    end
+    move_agent!(AnimalAgent, pos, animalModel)
+   # move_agent!(AnimalAgent, (rand(animalModel.rng, (0:calf_range)), rand(animalModel.rng, (0:calf_range)),0), animalModel; ifempty = false)
 elseif AnimalAgent.stage == :W
 elseif AnimalAgent.stage == :H
 elseif AnimalAgent.stage == :DH
