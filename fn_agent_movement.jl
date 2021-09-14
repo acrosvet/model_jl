2ab1cb90ca8102ae6cdb9bde879043444d86a932
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-15 10:17:37 +1200

    walk calf agents

--- fn_agent_movement.jl
+++ fn_agent_movement.jl
@@ -1,7 +1,7 @@
 function agent_movement!(AnimalAgent, animalModel)
 
 if AnimalAgent.stage == :C
-    
+    walk!(AnimalAgent), (rand(animalModel.rng(-10:10)), rand(animalModel.rng(-10:10)),0), animalModel; ifempty = false)
 elseif AnimalAgent.stage == :W
 elseif AnimalAgent.stage == :H
 elseif AnimalAgent.stage == :DH
