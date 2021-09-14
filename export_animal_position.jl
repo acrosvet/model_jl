83c22d3a18f790bf83b164b89e0133a409715133
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-14 17:14:16 +1200

    add stage

--- export_animal_position.jl
+++ export_animal_position.jl
@@ -1,6 +1,9 @@
-position_data = DataFrame(
+pos_data = DataFrame(
+    stage = AnimalAgent.stage,
     x = AnimalAgent.pos[1],
     y = AnimalAgent.pos[2],
     z = AnimalAgent.pos[3]
 )
-
+pos_output = open("./export/seasonal_model_run.csv","a")
+CSV.write(pos_output, pos_data, delim = ",", append = true, header = false)
+close(output)
