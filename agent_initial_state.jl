83c0e9d90bad8eb9256394956792aad08ee2f375
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-14 16:20:54 +1200

    add batch and split

--- agent_initial_state.jl
+++ agent_initial_state.jl
@@ -1,18 +1,54 @@
-tpm = initialiseSeasonal(100)
+# Seasonal  ------------------------------------
+init_pos = initialiseSeasonal(100)
 
 x = []
 y = []
 z = []
 
+
 for i in 1:length(tpm.agents)
-    push!(x, tpm[i].pos[1])
-    push!(y, tpm[i].pos[2])
-    push!(z, tpm[i].pos[3])
+    push!(x, init_pos[i].pos[1])
+    push!(y, init_pos[i].pos[2])
+    push!(z, init_pos[i].pos[3])
 end
 
 df = DataFrame(x = x, y = y, z = z)
 
-CSV.write("./export/initial_positions.csv", df)
-output = open("./export/seasonal_model_run.csv","w")
-    CSV.write(output, header, delim = ",", append = true, header = true)
-close(output)
+CSV.write("./export/seasonal_initial_positions.csv", df)
+
+# Split ------------------------------------
+init_pos = initialiseSplit(100)
+
+x = []
+y = []
+z = []
+
+
+for i in 1:length(tpm.agents)
+    push!(x, init_pos[i].pos[1])
+    push!(y, init_pos[i].pos[2])
+    push!(z, init_pos[i].pos[3])
+end
+
+df = DataFrame(x = x, y = y, z = z)
+
+CSV.write("./export/split_initial_positions.csv", df)
+
+# Seasonal  ------------------------------------
+init_pos = initialiseBatch(100)
+
+x = []
+y = []
+z = []
+
+
+for i in 1:length(tpm.agents)
+    push!(x, init_pos[i].pos[1])
+    push!(y, init_pos[i].pos[2])
+    push!(z, init_pos[i].pos[3])
+end
+
+df = DataFrame(x = x, y = y, z = z)
+
+CSV.write("./export/seasonal_initial_positions.csv", df)
+
