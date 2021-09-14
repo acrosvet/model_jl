b24a98361c1f6003ded818754e0cb92d219322d8
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-14 16:54:09 +1200

    Remove unused files

--- plot_pop_dynamics.jl
+++
@@ -1,9 +0,0 @@
-figure = Figure()
-ax = figure[1, 1] = Axis(figure; ylabel = "Number of animals")
-l1 = lines!(ax, simRun[:, dataname((:stage, stage_c))], color = :orange)
-l2 = lines!(ax, simRun[:, dataname((:stage, stage_w))], color = :green)
-l3 = lines!(ax, simRun[:, dataname((:stage, stage_h))], color = :red)
-l4 = lines!(ax, simRun[:, dataname((:stage, stage_l))], color = :black)
-
-
-figure  
