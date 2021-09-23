0ce5f8331dd859adbfdab676143a610b864f321d
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-24 08:48:48 +1200

    comment out statsbase, datastructures

--- packages.jl
+++ packages.jl
@@ -6,8 +6,8 @@ using InteractiveDynamics #Plot and animate ABMs
 using CairoMakie #Backend for video creationusing SimpleWeightedGraphs: SimpleWeightedDiGraph # will make social network
 using SparseArrays: findnz                        # for social network connections
 using LightGraphs
-using StatsBase
-using DataStructures
+#= using StatsBase
+using DataStructures =#
 using Distributions
 using DataFrames
 using Dates #Package for working with dates
