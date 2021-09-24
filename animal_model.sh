189d5710548a5a5aeb4290b5adde4de8dbf90405
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-24 21:09:12 +1200

    parallel

--- animal_model.sh
+++ animal_model.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH -p physical
-#SBATCH --time=01:00:00
+#SBATCH --time=00:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mail-user=acrosbie@student.unimelb.edu.au
-#SBATCH --mail-type=ALL
+#SBATCH --cpus-per-task=6
+
 
 module purge
 module load julia/1.5.1-linux-x86_64
-julia --threads 16 /data/gpfs/projects/punim0549/acrosbie/julia_abm/model_jl/scratchpad.jl
+julia --threads 16 /data/gpfs/projects/punim0549/acrosbie/model_jl/scratchpad.jl
 
 
 
--- scratchpad.jl
+++ scratchpad.jl
@@ -1,13 +1,13 @@
 using Distributed 
 
-addprocs(4)
+addprocs(5)
 
-@everywhere include("testing.jl")
+include("testing.jl")
 
 @everywhere tmp = initialiseSeasonal(220)
 
 
-#=  header = DataFrame(
+ header = DataFrame(
     Day = 0,
     ModelStep = 0,
     FarmID = 0,
@@ -82,10 +82,10 @@ culling_header = DataFrame(
 culling_output = open("./export/seasonal_culling.csv", "w")
     CSV.write(culling_output, culling_header, delim = ",", append = true, header = true)
 close(culling_output)
- =#
-adata = [:pos, :age, :status, :treatment, :inf_days, :days_exposed, :days_treated, :bactopop_r, :bactopop_is, :stage, :dim, :lactation, :pregstat, :dic, :stress, :days_recovered]
+
+#= adata = [:pos, :age, :status, :treatment, :inf_days, :days_exposed, :days_treated, :bactopop_r, :bactopop_is, :stage, :dim, :lactation, :pregstat, :dic, :stress, :days_recovered]
 mdata = [:date, :num_weaned, :num_heifers, :current_lac, :current_weaned, :current_dry, :current_calves, :current_heifers]
-@time @everywhere agent_data, model_data, _ = run!(tmp, agent_step!, model_step!, 100; adata, mdata) 
+ =#@time @everywhere agent_data, model_data, _ = run!(tmp, agent_step!, model_step!, 100; adata, mdata) 
 
 CSV.write("./export/agent_data.csv", agent_data)
 CSV.write("./export/model_data.csv", mdata)
