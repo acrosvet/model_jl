71fd1b6b1120a1ff08716f9a02221d9e0e1b0ca9
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-27 09:08:12 +1300

    output message

--- fn_trading_need.jl
+++ fn_trading_need.jl
@@ -7,7 +7,11 @@ function trading_need!(animalModel)
     animalModel.tradeable_weaned = animalModel.num_weaned - animalModel.current_weaned
     animalModel.tradeable_calves = animalModel.num_calves - animalModel.current_calves
     animalModel.tradeable_lactating = animalModel.num_lac - animalModel.current_lac
-    animalModel.tradeable_stock = sum(animalModel.tradeable_heifers + animalModel.tradeable_lactating + animalModel.tradeable_weaned + animalModel.tradeable_calves)
+    animalModel.tradeable_stock = animalModel.tradeable_heifers + animalModel.tradeable_lactating + animalModel.tradeable_weaned + animalModel.tradeable_calves
+
+    println("Tradeable stock")
+    println(animalModel.tradeable_stock)
 
 
 end
+ 
