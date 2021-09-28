d45a58386605b56baebb0e18c57f235f509e446c
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-28 13:26:23 +1300

    remove multithread

--- mstep_farm.jl
+++ mstep_farm.jl
@@ -6,7 +6,7 @@ farmModel.step += 1
 
 for a in allagents(farmModel)
     a.traded = false
-    Threads.@spawn step!(a.animalModel, agent_step!, model_step!, 1)
+    step!(a.animalModel, agent_step!, model_step!, 1)
 end
 
 
