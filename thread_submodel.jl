59a44d673800960037c4f82b692f2347c41871b4
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-29 12:16:53 +1300

    push hpc run

--- thread_submodel.jl
+++ thread_submodel.jl
@@ -2,10 +2,9 @@ function thread_submodel!(animalModel)
 
 
 
-     #Threads.@threads 
-     for a in collect(allagents(animalModel))
+     Threads.@threads for a in collect(allagents(animalModel))
          a.submodel.seed = a.id + animalModel.seed
-         Threads.@spawn step!(a.submodel, bact_agent_step!, bact_model_step!,1)
+          step!(a.submodel, bact_agent_step!, bact_model_step!,1)
      end
  
  end
