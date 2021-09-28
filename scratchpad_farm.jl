81e8ef3d2b61c4f5c7a54f84581995fdefb2d8dc
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-09-29 09:25:55 +1300

    seed error

--- scratchpad_farm.jl
+++ scratchpad_farm.jl
@@ -13,6 +13,6 @@ include("trade_header.jl")
 
 #@time run!(tmp, agent_step!, model_step!, 365) 
 
-tmp = initialiseFarms(numfarms = 100, nbact = 10000, dim = 100)
+tmp = initialiseFarms(numfarms = 50, nbact = 10000, dim = 100)
 
 @time run!(tmp, farm_step!, farm_mstep!, 5)
--- thread_submodel.jl
+++ thread_submodel.jl
@@ -1,10 +1,7 @@
 function thread_submodel!(animalModel)
-
-
-
-     
+   
     Threads.@spawn for a in allagents(animalModel)
-        a.submodel.seed = a.id
+        a.submodel.seed = a.id + a.seed
         step!(a.submodel, bact_agent_step!, bact_model_step!,1)
     end
 
