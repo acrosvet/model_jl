272ce19b0c4d444c6b735363e9ef4e087dec8a06
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-10-07 08:25:35 +1300

    farm hash

--- mstep_farm.jl
+++ mstep_farm.jl
@@ -5,6 +5,7 @@ farmModel.date += Day(1)
 farmModel.step += 1
 
 for a in allagents(farmModel)
+    a.rng = MersenneTwister(hash(a))
     a.traded = false
      step!(a.animalModel, agent_step!, model_step!, 1)
 end
