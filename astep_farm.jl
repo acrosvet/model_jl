9bea0880785a034429e282529c2f53e59d80af60
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-10-11 15:18:36 +1300

    return farm stepping to serial

--- astep_farm.jl
+++ astep_farm.jl
@@ -2,6 +2,9 @@ function farm_step!(FarmAgent, farmModel)
 
    # FarmAgent.animalModel.seed = FarmAgent.id
     
+   AnimalAgent.animalModel.rng = MersenneTwister(hash(AnimalAgent))
+   AnimalAgent.traded = false
+   step!(AnimalAgent.animalModel, agent_step!, model_step!, 1)
 
     if Dates.format(farmModel.date, "e") == "Wed" && FarmAgent.traded == false
         daytrader!(FarmAgent)
--- mstep_farm.jl
+++ mstep_farm.jl
@@ -4,11 +4,11 @@ function farm_mstep!(farmModel)
 farmModel.date += Day(1)
 farmModel.step += 1
 
-Threads.@threads for a in collect(allagents(farmModel))
+#= Threads.@threads for a in collect(allagents(farmModel))a
     a.animalModel.rng = MersenneTwister(hash(a))
     a.traded = false
     step!(a.animalModel, agent_step!, model_step!, 1)
-end 
+end  =#
 
 
 end
