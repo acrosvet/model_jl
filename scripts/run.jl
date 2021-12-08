9a72d807f9bc4973cb14ae56c3e5464c7a797511
acrosvet <34050682+acrosvet@users.noreply.github.com>
2021-12-09 09:33:47 +1300

    create run script

--- scripts/run.jl
+++ scripts/run.jl
@@ -1,15 +1,17 @@
-
 include("./src/animal_na.jl")
 
-
-@time animal_step!(animalModel, animalData)
-#@profview animal_step!(animalModel, animalData)
-
-
-
 #@profview 
-@time [animal_step!(animalModel, animalData) for i in 1:1825]
+#@time [animal_step!(animalModel, animalData) for i in 1:365*10]
+
+runs = []
+
+for i in 1:10
+  [animal_step!(animalModel, animalData) for j in 1:365]
+  push!(runs, animalData)
+end
 
 @time export_animalData!(animalData)
 
 write_allData!(allData)
+
+write("data", runs)
